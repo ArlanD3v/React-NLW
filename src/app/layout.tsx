26929d0fe6679c0f15747643d2850a9577deed4a
Arlan <arland3v@gmail.com>
2025-02-24 20:48:11 -0300

    Pull

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -3,30 +3,30 @@ import type { Metadata } from 'next';
 import { Montserrat, Oxanium } from 'next/font/google';
 
 export const metadata: Metadata = {
-  title: 'Progress',
+	title: 'Progress',
 };
 
 const oxanium = Oxanium({
-  weight: ['500', '600'],
-  subsets: ['latin'],
-  variable: '--font-oxanium',
+	weight: ['500', '600'],
+	subsets: ['latin'],
+	variable: '--font-oxanium',
 });
 
 const montserrat = Montserrat({
-  weight: ['500', '600'],
-  subsets: ['latin'],
-  variable: '--font-montserrat',
+	weight: ['500', '600'],
+	subsets: ['latin'],
+	variable: '--font-montserrat',
 });
 
 export default function RootLayout({
-  children,
+	children,
 }: Readonly<{
-  children: React.ReactNode;
+	children: React.ReactNode;
 }>) {
-  return (
-    <html lang="pt-BR" className={`${oxanium.variable} ${montserrat.variable}`}>
-      <body className="bg-black text-white text-4xl font-bold">{children}</body>
-    </html>
-  );
+	return (
+		<html lang="pt-BR" className={`${oxanium.variable} ${montserrat.variable}`}>
+			<body className="bg-black text-white text-4xl font-bold">{children}</body>
+		</html>
+	);
 }
 /* Teste */
