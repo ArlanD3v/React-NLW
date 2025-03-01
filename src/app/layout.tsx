fbc4aea8537ac4795cb1d584c2c7d3ae6cc5cb25
Arlan <arland3v@gmail.com>
2025-02-28 23:18:43 -0300

    Fix: Ajustes

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -3,7 +3,6 @@
 import './globals.css'
 import { MousePointer2 } from 'lucide-react'
 import { Montserrat, Oxanium } from 'next/font/google'
-import Head from 'next/head'
 import { useEffect, useState } from 'react'
 
 const oxanium = Oxanium({
@@ -24,8 +23,10 @@ export default function RootLayout({
   children: React.ReactNode
 }) {
   const [cursorPos, setCursorPos] = useState({ x: 0, y: 0 })
+  const [isClient, setIsClient] = useState(false)
 
   useEffect(() => {
+    setIsClient(true)
     const handleMouseMove = (e: MouseEvent) => {
       setCursorPos({ x: e.clientX, y: e.clientY })
     }
@@ -35,25 +36,28 @@ export default function RootLayout({
 
   return (
     <html lang="pt-BR" className={`${oxanium.variable} ${montserrat.variable}`}>
-      <Head>
-        <title>Progress</title>
-        <meta name="description" />
-      </Head>
+      <head>
+        <title>Rank Dev</title>
+        <link rel="shortcut icon" href="/d.ico" type="image/x-icon" />
+        <meta name="description" content="Descrição da página" />
+      </head>
       <body className="bg-gray-900 text-gray-100 antialiased bg-[url(/Background.png)] bg-no-repeat bg-top md:bg-right-top">
         <main className="max-w-[1240px] mx-auto px-5 py-8 md:py-0">
           {children}
         </main>
 
-        <MousePointer2
-          className="fixed text-yellow-400 pointer-events-none"
-          style={{
-            left: cursorPos.x,
-            top: cursorPos.y,
-            position: 'fixed',
-            transform: 'translate(-50%, -50%)',
-          }}
-          size={25}
-        />
+        {isClient && (
+          <MousePointer2
+            className="fixed text-yellow-400 pointer-events-none"
+            style={{
+              left: cursorPos.x,
+              top: cursorPos.y,
+              position: 'fixed',
+              transform: 'translate(-50%, -50%)',
+            }}
+            size={25}
+          />
+        )}
       </body>
     </html>
   )
